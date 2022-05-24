cb70b65c6a4a4e20669819970cdf473b2fabb889
aratirananaware <aratirananaware24@gmail.com>
2022-05-24 13:05:55 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/palindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the string");
+            string s = Console.ReadLine();
+            char [] t=s.ToCharArray();
+
+
+            
+            Array.Reverse(t);
+           string rev = new string(t);
+            bool flag = false;
+            if(s.Equals(rev))
+            {
+                flag = true;
+            }
+            
+            
+            
+
+            
+            if(flag)
+            {
+                Console.WriteLine("string is palindrome : " + s);
+            }
+            else
+            {
+                Console.WriteLine("string is not palindrome");
+            }
+        }
+    }
+}
